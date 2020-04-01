91feeefcb03f148f5e69d8882502751ee1b3ccb7
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2020-03-31 21:29:31 -0300

    moble da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,20 @@
+import 'intl'
+import 'intl/locale-data/jsonp/pt-BR'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
